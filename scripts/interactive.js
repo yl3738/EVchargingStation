219fbfef0fa47b21a28d77c65ea09c30ed7f7612
yl3738 <yl3738@columbia.edu>
2020-12-16 20:31:10 -0500

    add interative

--- scripts/interactive.js
+++ scripts/interactive.js
@@ -15,6 +15,7 @@
   var innerHeight = h - margin.top - margin.bottom;
 
   var bardata = [40, 10, 11, 28, 27, 171, 112, 175, 234];
+  
 
   var xScale = d3.scaleBand()
       .domain(d3.range(bardata.length))
@@ -75,3 +76,66 @@
       .attr("transform", `translate (${margin.left}, ${margin.top})`)
       .call(yAxis);
 
+
+
+
+// General Update Pattern
+  function update(data) {
+      xScale.domain(d3.range(data.length));
+
+      yScale.domain([0, d3.max(data)]);
+
+      var bars = svg.select("g")
+          .selectAll("rect")
+          .data(data);
+
+      bars.enter()
+        .append("rect")
+          .attr("x", w)
+          .attr("y", d => yScale(d))
+          .attr("width", xScale.bandwidth())
+          .attr("height", d => innerHeight - yScale(d))
+          .attr("fill", "blue")
+        .merge(bars)
+          .transition()
+          .duration(2000)
+          .ease(d3.easeLinear)
+          .attr("x", (d, i) => xScale(i))
+          .attr("y", d => yScale(d))
+          .attr("width", xScale.bandwidth())
+          .attr("height", d => innerHeight - yScale(d))
+
+      bars.exit().remove();
+
+      svg.select(".xAxis")
+          .transition()
+          .duration(2000)
+          .ease(d3.easeLinear)
+          .call(xAxis);
+
+      svg.select(".yAxis")
+          .transition()
+          .duration(2000)
+          .ease(d3.easeLinear)
+          .call(yAxis);
+    }
+    
+    
+    
+  // Interactivity
+
+    d3.selectAll("p")
+        .on("click", function() {
+
+            var paraID = d3.select(this).attr("id");
+
+            if (paraID == "change") {
+                bardata = [1.61, 34.57, 45.32, 46.44, 37.45, 65.02, 119.98, 172.06, 123.43];
+
+            } ;
+
+            update(bardata);
+
+        });
+    
+    
